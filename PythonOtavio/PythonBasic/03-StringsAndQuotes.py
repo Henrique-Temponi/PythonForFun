72d1f4d7ddce89d2e3985b83d1e0de7cd009f863
Henrique Temponi <tanzenheavely@gmail.com>
2019-08-17 13:16:28 -0300

    add string

--- PythonOtavio/PythonBasic/03-StringsAndQuotes.py
+++ PythonOtavio/PythonBasic/03-StringsAndQuotes.py
@@ -1,3 +1,14 @@
 """
+today we will learn a little more about string
+"""
 
+"""
+so in a nutshell String is everything between '' or "", treat it as a text,
+so keep in mind that while working with string that it should be text to text
+e.g: "hello" + 1
+that we are saying there is do the operation + with a string and a int,
+and it won't work because string works with text + text
+a solution would be 
+"hello" + "1"
+this is ok
 """
