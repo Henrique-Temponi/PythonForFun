4ac0194be68d46699446249b91b1e9f338bbf2a0
Henrique Temponi <tanzenheavely@gmail.com>
2019-08-17 12:19:35 -0300

    i

---
+++ PythonOtavio/PythonBasic/01-Comentarios.py
@@ -0,0 +1,27 @@
+# Python has a few ways to comment, one of them being with "#", which is what we are using right now to comment code
+
+print("Hello", end=" ")      # this will be printed to screen,
+# print("World")     # while this will not
+
+# We can also do documentation comments,
+"""
+Like so!
+"""
+
+# we also have single quoted comments
+
+'''
+Like this!
+'''
+
+# so recapping, use # for a single line comment, """ - for a doc comment, and a ''' - for a multiple comment!
+
+'''
+Quiz time!
+which of the following print will be executed?
+if you want to check the answer just execute the program
+'''
+
+# print("Hello")
+print("World")
+
---
+++ PythonOtavio/PythonBasic/02-print.py
@@ -0,0 +1,7 @@
+"""
+So we will learn about the print function, we will just go over a few details
+"""
+
+'''
+So we will learn about the print function, we will just go over a few details
+'''
