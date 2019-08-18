f08b334c2442d06093aa9915b6d6edd086591215
Henrique Temponi <tanzenheavely@gmail.com>
2019-08-18 18:22:01 -0300

    added input

---
+++ PythonOtavio/PythonBasic/08-Input.py
@@ -0,0 +1,11 @@
+"""
+getting input in python,
+the input method is quite simple
+just use input, and then it will read a input from the user, returning a string
+NOTE: it's quite always a input
+"""
+
+x = input("input a number: ")
+print(x, type(x))
+
+# if you want to convert it's value just remember to use the type casting
