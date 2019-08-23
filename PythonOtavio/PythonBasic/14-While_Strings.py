f56027866aa09a388d55bcfadad2d1ef34efa71c
henrique-temponi <tanzenheavely@gmail.com>
2019-08-23 09:20:16 -0300

    fixed missing info

--- PythonOtavio/PythonBasic/14-While_Strings.py
+++ PythonOtavio/PythonBasic/14-While_Strings.py
@@ -11,16 +11,44 @@ let see some examples
 first let's try to count the total number of letters
 """
 
-x = "banana"
+x = "bananaa"
 tam = len(x)  # this will get the total length of given string,
 count = 0
 c = 0
 
 while c < tam:
 
-    if x[c] != '':
+    if x[c] != '':  # the useage of [] in this case is to get index of a string, 
+                    # remembering that a string have a index, so we can access that by using a []
         count += 1
 
     c += 1
 
 print(f"The string have {count} letters")
+
+
+"""
+let's try something different, 
+now we want to check what is most used letter
+in a given string
+"""
+c = 0
+letter_most_used = ""
+number_times = 0
+
+while c < tam:
+    
+    current_letter = x[c]
+
+    current_number = x.count(x[c])
+
+    if current_number > number_times:
+
+        letter_most_used = x[c]
+        number_times = current_number
+
+    # print(x[c])
+    c += 1
+    
+
+print(f"The letter most used was: '{letter_most_used}' it appeared {number_times} times")
