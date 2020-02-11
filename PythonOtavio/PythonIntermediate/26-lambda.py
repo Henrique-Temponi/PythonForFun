4f43688e310290242b0bb714eec854caa6e3caf8
henrique-temponi <tanzenheavely@gmail.com>
2020-02-10 20:10:46 -0500

    Add Lambda expressions

---
+++ PythonOtavio/PythonIntermediate/26-lambda.py
@@ -0,0 +1,27 @@
+"""
+lambda expressions in python
+"""
+
+#
+# def func(arg, arg2):
+#     return arg * arg2
+#
+#
+# var = func(2, 2)
+#
+# a = lambda x, y: x * y
+# print(a(2, 2))
+
+lista = [
+    ['P1', 132],
+    ['P2', 142],
+    ['P3', 146],
+    ['P4', 174],
+    ['P5', 148],
+]
+
+# lista.sort(key=lambda i: i[1])
+
+
+
+print(lista)
